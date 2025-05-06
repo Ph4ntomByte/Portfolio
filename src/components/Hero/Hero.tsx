5dbb2cf69111c92c12cd946fea3bd5550f697b65
D3f4ult <cvdjb8@inf.elte.hu>
2025-05-06 17:49:43 +0200

    initial commit

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,64 @@
+'use client'
+import useRoleSwitcher from '@/hooks/useRoleSwitcher'
+import useRotatingAnimation from '@/hooks/useRotatingAnimation'
+import Image from 'next/image'
+import { HeroImage } from '../../utils/images'
+import Ellipse from './Ellipse'
+
+const Hero = () => {
+  const ellipseRef = useRotatingAnimation()
+  const role = useRoleSwitcher({ roles: ['FULL-STACK DEVELOPER', 'CS STUDENT', 'INNOVATOR'] })
+
+  return (
+    <section className="bg-primary bg-small-glow bg-small-glow-position md:bg-large-glow-position lg:bg-large-glow min-h-[calc(dvh-4rem)] bg-no-repeat">
+      <div className="mx-auto grid max-w-[1200px] grid-cols-1 items-center gap-4 px-4 pt-12 pb-10 md:grid-cols-2 lg:p-4">
+        <div className="flex min-h-48 flex-col justify-between lg:min-h-56 lg:max-w-[33.75rem]">
+          <h1>
+            <span className="text-neutral mb-2 block text-3xl font-bold">Hi, I’m Rauf</span>
+            <span className="text-accent block text-[1.75rem] font-bold">{role}</span>
+          </h1>
+
+          <h2 className="text-neutral mt-3">
+            Building modern web apps and solving complex problems
+          </h2>
+
+          <div className="mt-6 flex flex-wrap gap-6">
+            <a
+              href="#contact"
+              aria-label="Contact me"
+              className="bg-accent min-w-32 cursor-pointer rounded-lg px-[14px] py-[10px] text-center text-sm font-medium text-[#00071E]"
+            >
+              Contact Me
+            </a>
+            <a
+              href="https:linkedin.com/in/rauf-suleymanov-73409a296"
+              aria-label="View LinkedIn Profile"
+              className="text-neutral bg-secondary cursor-pointer rounded-lg px-[14px] py-[10px] text-sm"
+            >
+              LinkedIn Profile
+            </a>
+          </div>
+        </div>
+
+        <div className="flex min-h-[18.75rem] items-center justify-center lg:min-h-[35rem]">
+          <div className="text-accent relative size-56 sm:size-60 md:size-[20rem] lg:size-[25.75rem]">
+            <Image
+              src={HeroImage}
+              fill
+              priority
+              sizes="(min-width: 1024px) 25.75rem, (min-width: 768px) 20rem, (min-width: 640px) 15rem, 14rem"
+              alt="Rauf – Full‑Stack Developer"
+              className="object-contain p-7"
+            />
+            <Ellipse
+              ref={ellipseRef}
+              className="absolute top-0 left-0 size-56 transition-transform duration-500 ease-out sm:size-60 md:size-[20rem] lg:size-[25.75rem]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
